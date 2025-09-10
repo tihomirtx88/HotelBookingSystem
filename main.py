e42a4e81c2c7214c7c33612d618a3e34b9928a11
tihomirtx88 <tihomirtx88@gmail.com>
2025-09-10 21:02:33 +0300

    Implement credit card feature

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas
 from numpy.ma.core import squeeze
 
 df = pandas.read_csv("hotels.csv", dtype={"id":str});
+df_cards = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records");
 
 class Hotel:
     def __init__(self, hotel_id):
@@ -35,15 +36,28 @@ class ReservationTickets:
         """
         return conten;
 
-print(df)
+class CreditCard:
+    def __init__(self, number):
+        self number = number;
+
+    def validate(self, expiration, holder, cvc):
+        cad_data = {"number": self.number, "expiration": expiration, "holder": holder, "cvc": cvc};
+        if cad_data in df_cards:
+            return True;
+        else:
+            return False;
 
 hotel_ID = input("Add the id of the hotel: ")
 hotel = Hotel(hotel_ID);
 
 if hotel.available():
-    hotel.book();
-    name = input("Enter your name: ");
-    reservation_ticket = ReservationTickets(customer_name=name, hotel_object=hotel);
-    print(reservation_ticket.generate());
+    credit_card = CreditCard( number="2345675678");
+    if credit_card.validate( expiration="08.12.2025", holder="Asen Asen", cvc="234"):
+        hotel.book();
+        name = input("Enter your name: ");
+        reservation_ticket = ReservationTickets(customer_name=name, hotel_object=hotel);
+        print(reservation_ticket.generate());
+    else:
+        print("There is problem with payment")
 else:
     print("hotel is not free");
