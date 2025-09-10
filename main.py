4972a09f92eec69c488838bc2d79b2c8fd6dce88
tihomirtx88 <tihomirtx88@gmail.com>
2025-09-10 16:18:17 +0300

    create instances

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-class User:
-    pass;
+import pandas
 
 class Hotel:
-    pass;
+    def __init__(self, id):
+        pass;
+
+    def book(self):
+        pass
+
+    def available:
+        pass
 
 class ReservationTickets:
-    pass;
+    def __init__(self, customer_name, hotel_object):
+        pass;
+
+    def generate(self):
+        pass;
+
+print(df)
+
+id = input("Add the id of the hotel: ")
+hotel = Hotel(id);
+
+if hotel.available():
+    hotel.book();
+    name = input("Enter your name: ");
+    reservation_ticket = ReservationTickets(name, hotel);
+    print(reservation_ticket.generate());
+else:
+    print("hotel is not free");
